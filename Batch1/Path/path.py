394a859fc2d80bb8917a33b9a7b5f9dffc4bbc5f
vamel <79980481+vamel@users.noreply.github.com>
2021-10-15 22:29:15 +0200

    Merged Duckonaut

--- Batch1/Path/path.py
+++ Batch1/Path/path.py
@@ -1,11 +1,19 @@
-def read_values():
-    values = []
-    with open("example.txt") as file_handle:
-        for line in file_handle:
-            temp_list = list(line)
-            t_list = [number for number in temp_list if number.isnumeric()]
-            values.append(t_list)
-    return values
+from typing import List
+
+
+def get_data(s: str) -> List[int]:
+    lastSpace = False
+    formatted = ''
+    
+    for c in s:
+        if c == ' ' and not lastSpace:
+            formatted += ' '
+            lastSpace = True
+        elif c.isnumeric():
+            formatted += c
+            lastSpace = False
+
+    return [int(n) for n in formatted.split(' ')]
 
 def calculate_min(values):
     result = [None] * len(values)
@@ -32,7 +40,14 @@ def calculate_max(values):
     return result[0]
 
 def main():
-    values = read_values()
+    values = []
+    with open("example.txt") as file_handle:
+        for line in file_handle:
+            t_list = get_data(line)
+            values.append(t_list)
     min_result = calculate_min(values)
     max_result = calculate_max(values)
     print(min_result, max_result)
+
+if __name__ == '__main__':
+    main()
